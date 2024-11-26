a4d5e70c285b95ba9b641d1daf05c68d5940abbe
Snorlax0815 <mrafeiner@student.tgm.ac.at>
2024-11-26 13:10:41 +0100

    added new news to news section

--- Projektwebseite/src/views/AboutView.vue
+++ Projektwebseite/src/views/AboutView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class=" h-100 container-fluid ms-0 ps-0 overflow-x-scroll" style="width: 100%">
     <div class="row w-100 h-100" >
-      <div class="col-xl-8 col-md-7 col-sm-12 col-12 h-100 ">
+      <div class="col-xl-5 col-md-5 col-sm-12 col-12 h-100 ">
         <div class="w-100 h-100">
           <div class="card w-100 h-100 mh-100">
             <div class="card-header">
@@ -28,16 +28,38 @@
           </div>
         </div>
       </div>
-      <div class="col-4 col-md-4 col-sm-12 col-12 h-100">
+      <div class="col-7 col-md-7 col-sm-12 col-12 h-100">
         <div class="w-100 h-100 ">
           <div class="card w-100 h-100 mh-100 mw-100">
             <div class="card-header">
               <h5 class="card-subtitle">Informationen und Aktuelles</h5>
             </div>
             <div class="card-body overflow-y-auto">
+
               <div class="card m-3 shadow">
                 <div class="card-img-top img-fluid d-flex ">
-                  <img src="@/assets/qrcode_wildeye_tech.png" alt="logo" class="ms-auto me-auto mw-100 mh-100 rounded rounded-3">
+                  <img src="@/assets/logo_v2_1.png" alt="Neues Logo" class="ms-auto me-auto mw-100 mh-100 rounded rounded-3">
+                </div>
+                <div class="card-header">
+                  <div>
+                    <p class="fs-5">Neues Logo! - 26.11.2024</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="card m-3 shadow">
+                <div class="card-img-top img-fluid d-flex ">
+                  <img src="@/assets/mockup_new_website.png" alt="Mockup neue Webseite" class="ms-auto me-auto mw-100 mh-100 rounded rounded-3">
+                </div>
+                <div class="card-header">
+                  <div>
+                    <p class="fs-5">Neues Mockup und Design! - 26.11.2024</p>
+                  </div>
+                </div>
+              </div>
+              <div class="card m-3 shadow">
+                <div class="card-img-top img-fluid d-flex ">
+                  <img src="@/assets/qrcode_wildeye_tech.png" alt="website qrcode" class="ms-auto me-auto mw-100 mh-100 rounded rounded-3">
                 </div>
                 <div class="card-header">
                   <div>
@@ -46,8 +68,8 @@
                 </div>
               </div>
               <div class="card m-3 shadow">
-                <div class="card-img-top img-fluid d-flex ">
-                  <img src="@/assets/website_v1.png" alt="logo" class="ms-auto me-auto mw-100 mh-100 rounded rounded-3">
+                <div class="card-img-top img-fluid d-flex">
+                  <img src="@/assets/website_v1.png" alt="website" class="ms-auto me-auto mw-100 mh-100 rounded rounded-3">
                 </div>
                 <div class="card-header">
                   <div>
