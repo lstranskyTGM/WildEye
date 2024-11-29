fa54e23a264b70122d2ffea81015d974761df406
Snorlax0815 <mrafeiner@student.tgm.ac.at>
2024-11-29 17:25:13 +0100

    added material web components, css variables.

--- WebApp/src/App.vue
+++ WebApp/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="vh-100 vw-100 overflow-hidden edge_color">
     <div class="row-cols-2 d-flex w-100 h-100" style="padding-top: 2%">
-      <div class="col-xxl-1 col-xl-1 col-lg-1 col-md-2 col-sm-12 edge_color">
+      <div class="col-xxl-1 col-xl-1 col-lg-1 col-md-2 col-sm-3 col-3 edge_color">
         <div class="d-flex h-100 w-100">
           <div class="row-cols-1 ms-auto me-auto h-100">
             <nav class="mt-0 h-100 w-100 align-content-start d-flex flex-column">
@@ -9,63 +9,33 @@
                 <img src="@/assets/logo_v2_1.png" class="card-img-top img-fluid" style="object-fit: contain; width: 70%;">
               </div>
               <div class="overflow-y-auto h-100 flex-grow-1">
-                <div class="col">
-                  <router-link to="/" class="text-decoration-none text-reset text-center d-grid">
-                    <div class="ratio ratio-1x1 bg-body rounded-3 w-75 ms-auto me-auto " style="height: 100%; width: 100%;">
-                      <div class="d-flex justify-content-center align-items-center w-100 h-100 ">
-                        <div class="shadow justify-content-center align-content-center rounded-3" style="width: 83%; height: 90%;">
-                          <i class="bi bi-kanban w-100 d-block" style="font-size: 2rem; color: #DA785DFF;"></i>
-                          <small class="w-100">Dashboard</small>
-                        </div>
-                      </div>
-                    </div>
+                <div class="col mt-3">
+                  <router-link to="/" class="text-decoration-none text-reset text-center d-grid" @click="this.setActiveRoute('/')">
+                    <md-elevated-button v-if="activeRoute === '/'" class="w-75 ms-auto me-auto"><i class="bi bi-kanban fs-3"></i></md-elevated-button>
+                    <md-text-button v-else class="w-75 ms-auto me-auto" ><i class="bi bi-kanban fs-3"></i></md-text-button>
+                    <p>Dashboard</p>
                   </router-link>
                 </div>
                 <div class="col mt-3">
-                  <router-link to="/picture" class="text-decoration-none text-reset text-center">
-                    <div class="ratio ratio-1x1 bg-body p-2 rounded-3 w-75 ms-auto me-auto " style="height: 100%; width: 100%;">
-                      <div class="d-flex justify-content-center align-items-center w-100 h-100 ">
-                        <div class="shadow justify-content-center align-content-center rounded-3" style="width: 83%; height: 90%;">
-                          <i class="bi bi-images w-100 d-block" style="font-size: 2rem; color: #DA785DFF"></i>
-                          <small class="w-100">Pictures</small>
-                        </div>
-                      </div>
-                    </div>
+                  <router-link to="/picture" class="text-decoration-none text-reset text-center d-grid" @click="this.setActiveRoute('/picture')">
+                    <md-elevated-button v-if="activeRoute === '/picture'" class="w-75 ms-auto me-auto"><i class="bi bi-images fs-3"></i></md-elevated-button>
+                    <md-text-button v-else class="w-75 ms-auto me-auto" ><i class="bi bi-images fs-3"></i></md-text-button>
+                    <p>Pictures</p>
                   </router-link>
                 </div>
                 <div class="col mt-3">
-                  <router-link to="/about" class="text-decoration-none text-reset text-center">
-                    <div class="ratio ratio-1x1 bg-body p-2 rounded-3 w-75 ms-auto me-auto " style="height: 100%; width: 100%;">
-                      <div class="d-flex justify-content-center align-items-center w-100 h-100 ">
-                        <div class="shadow justify-content-center align-content-center rounded-3" style="width: 83%; height: 90%;">
-                          <i class="bi bi-broadcast w-100 d-block" style="font-size: 2rem; color: #DA785DFF"></i>
-                          <small class="w-100">Live</small>
-                        </div>
-                      </div>
-                    </div>
+                  <router-link to="/map" class="text-decoration-none text-reset text-center d-grid" @click="this.setActiveRoute('/map')">
+                    <md-elevated-button v-if="activeRoute === '/map'" class="w-75 ms-auto me-auto"><i class="bi bi-map fs-3"></i></md-elevated-button>
+                    <md-text-button v-else class="w-75 ms-auto me-auto" ><i class="bi bi-map fs-3"></i></md-text-button>
+                    <p>Map</p>
                   </router-link>
                 </div>
                 <div class="col mt-3">
-                  <router-link to="/map" class="text-decoration-none text-reset text-center">
-                    <div class="ratio ratio-1x1 bg-body p-2 rounded-3 w-75 ms-auto me-auto " style="height: 100%; width: 100%;">
-                      <div class="d-flex justify-content-center align-items-center w-100 h-100 ">
-                        <div class="shadow justify-content-center align-content-center rounded-3" style="width: 83%; height: 90%;">
-                          <i class="bi bi-map w-100 d-block" style="font-size: 2rem; color: #DA785DFF"></i>
-                          <small class="w-100 pt-0 mt-0">Map</small>
-                        </div>
-                      </div>
-                    </div>
+                  <router-link to="/map" class="text-decoration-none text-reset text-center d-grid"  @click="this.setActiveRoute('/account')">
+                    <md-elevated-button v-if="activeRoute === '/account'" class="w-75 ms-auto me-auto"><i class="bi bi-person-rolodex fs-3"></i></md-elevated-button>
+                    <md-text-button v-else class="w-75 ms-auto me-auto"><i class="bi bi-person-rolodex fs-3"></i></md-text-button>
+                    <p>Account</p>
                   </router-link>
-                </div>
-                <div class="col mt-5">
-                  <div class="ratio ratio-1x1 bg-body p-2 rounded-3 w-75 ms-auto me-auto " style="height: 100%; width: 100%;">
-                    <div class="d-flex justify-content-center align-items-center w-100 h-100 ">
-                      <div class="shadow justify-content-center align-content-center rounded-3" style="width: 83%; height: 90%;">
-                        <i class="bi-0-circle-fill d-block" style="font-size: 2rem; color: #DA785DFF"></i>
-                        <small class="w-100">Account</small>
-                      </div>
-                    </div>
-                  </div>
                 </div>
               </div>
 
@@ -87,10 +57,49 @@
   </div>
 </template>
 
-<style>
-.edge_color{
-  background-color: #e7e7e7;
-}
-</style>
 <script>
+
+import "@material/web/all"
+import {styles as typescaleStyles} from '@material/web/typography/md-typescale-styles.js';
+export default {
+  name: 'App',
+  data(){
+    return{
+      activeRoute: '/'
+    }
+  },
+  methods: {
+    setActiveRoute(route){
+      this.activeRoute = route;
+    },
+    async showToast() {
+      const platform = Capacitor.getPlatform();
+      console.log('Current platform:', platform);
+      await Toast.show({
+        text: `Hello from ${platform}!`
+      });
+    },
+    async getLocation() {
+      const coordinates = await Geolocation.getCurrentPosition();
+      console.log('Current position:', coordinates);
+      await Toast.show({
+        text: `Current position: ${coordinates.coords.latitude}, ${coordinates.coords.longitude}`
+      });
+    }
+  },
+  beforeMount() {
+    document.adoptedStyleSheets = [typescaleStyles.styleSheet];
+  }
+}
 </script>
+
+<style>
+@import "../css/custom.css";
+#app, html, body {
+  font-family: Roboto, sans-serif;
+}
+.edge_color{
+  background-color: var(--wildeye-edge-color);
+}
+
+</style>
--- WebApp/vue.config.js
+++ WebApp/vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+    chainWebpack: (config) => {
+        config.module
+            .rule('vue')
+            .use('vue-loader')
+            .tap((options) => ({
+                ...options,
+                compilerOptions: {
+                    // treat any tag that starts with ion- as custom elements
+                    isCustomElement: (tag) => tag.startsWith('md-')
+                }
+            }))
+    }
 })
